77300849968765707426ffc5db535f903d73625b
crislainesc <criis.santos@outlook.com>
2024-01-15 20:19:27 -0300

    test: add models tests and factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
-require 'rspec/rails'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
